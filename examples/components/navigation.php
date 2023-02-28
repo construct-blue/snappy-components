5ae9f425bdc77f8844e738a827124582e9bb5db8
robertkleinschuster <robert.kleinschuster@niceshops.com>
2023-02-28 10:23:42 +0100

    HTML Components

--- examples/components/navigation.php
+++ examples/components/navigation.php
@@ -8,5 +8,6 @@ return fn() => yield <<<HTML
 <li><a href="/functional-components.php">Functional Components</a></li>
 <li><a href="/class-components.php">Class Components</a></li>
 <li><a href="/html-components.php">HTML Components</a></li>
+<li><a href="/slots.php">Slots</a></li>
 </ul>
 HTML;
